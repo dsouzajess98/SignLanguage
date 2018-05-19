9c7a256728d4c26afc13c2b86d824f08aea082e4
dsouzajess98 <djessica@student.nitw.ac.in>
2018-05-19 14:51:50 +0530

    Initial commit

---
+++ Windows/LiveCameraSample/MainWindow.xaml.cs
@@ -0,0 +1,322 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Cognitive Services: http://www.microsoft.com/cognitive
+// 
+// Microsoft Cognitive Services Github:
+// https://github.com/Microsoft/Cognitive
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using OpenCvSharp;
+
+using OpenCvSharp.Extensions;
+using VideoFrameAnalyzer;
+using Common = Microsoft.ProjectOxford.Common;
+using FaceAPI = Microsoft.ProjectOxford.Face;
+using VisionAPI = Microsoft.ProjectOxford.Vision;
+
+
+namespace LiveCameraSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private FaceAPI.FaceServiceClient _faceClient = null;
+        private VisionAPI.VisionServiceClient _visionClient = null;
+        private readonly FrameGrabber<LiveCameraResult> _grabber = null;
+        private static readonly ImageEncodingParam[] s_jpegParams = {
+            new ImageEncodingParam(ImwriteFlags.JpegQuality, 60)
+        };
+        private readonly CascadeClassifier _localFaceDetector = new CascadeClassifier();
+        private bool _fuseClientRemoteResults;
+        private LiveCameraResult _latestResultsToDisplay = null;
+        private AppMode _mode;
+        private DateTime _startTime;
+
+        public enum AppMode
+        {
+
+            Tags,
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create grabber. 
+            _grabber = new FrameGrabber<LiveCameraResult>();
+
+            // Set up a listener for when the client receives a new frame.
+            _grabber.NewFrameProvided += (s, e) =>
+            {
+
+
+                // The callback may occur on a different thread, so we must use the
+                // MainWindow.Dispatcher when manipulating the UI. 
+                this.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    // Display the image in the left pane.
+                    LeftImage.Source = e.Frame.Image.ToBitmapSource();
+
+                    // If we're fusing client-side face detection with remote analysis, show the
+                    // new frame now with the most recent analysis available. 
+                    if (_fuseClientRemoteResults)
+                    {
+                        RightImage.Source = VisualizeResult(e.Frame);
+                    }
+                }));
+
+                // See if auto-stop should be triggered. 
+                if (Properties.Settings.Default.AutoStopEnabled && (DateTime.Now - _startTime) > Properties.Settings.Default.AutoStopTime)
+                {
+                    _grabber.StopProcessingAsync();
+                }
+            };
+
+            // Set up a listener for when the client receives a new result from an API call. 
+            _grabber.NewResultAvailable += (s, e) =>
+            {
+                this.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    if (e.TimedOut)
+                    {
+                        MessageArea.Text = "API call timed out.";
+                    }
+                    else if (e.Exception != null)
+                    {
+                        string apiName = "";
+                        string message = e.Exception.Message;
+                        var faceEx = e.Exception as FaceAPI.FaceAPIException;
+                        var emotionEx = e.Exception as Common.ClientException;
+                        var visionEx = e.Exception as VisionAPI.ClientException;
+                        if (faceEx != null)
+                        {
+                            apiName = "Face";
+                            message = faceEx.ErrorMessage;
+                        }
+                        else if (emotionEx != null)
+                        {
+                            apiName = "Emotion";
+                            message = emotionEx.Error.Message;
+                        }
+                        else if (visionEx != null)
+                        {
+                            apiName = "Computer Vision";
+                            message = visionEx.Error.Message;
+                        }
+                        MessageArea.Text = string.Format("{0} API call failed on frame {1}. Exception: {2}", apiName, e.Frame.Metadata.Index, message);
+                    }
+                    else
+                    {
+                        _latestResultsToDisplay = e.Analysis;
+
+                        // Display the image and visualization in the right pane. 
+                        if (!_fuseClientRemoteResults)
+                        {
+                            RightImage.Source = VisualizeResult(e.Frame);
+                        }
+                    }
+                }));
+            };
+
+            // Create local face detector. 
+            _localFaceDetector.Load("Data/haarcascade_frontalface_alt2.xml");
+        }
+
+        //Function to get the appropriate letter for the sign
+        private async Task<LiveCameraResult> TaggingAnalysisFunction(VideoFrame frame)
+        {
+
+            // Encode image. 
+            var jpg = frame.Image.ToMemoryStream(".jpg", s_jpegParams);
+
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "a062270b9eeb46109e41fbf97cf586d3");
+
+            // Prediction URL - replace this example URL with your valid prediction URL.
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/52f29cd5-8bd0-452e-b85e-e7a36a76fcee/image?iterationId=d333be81-2606-487d-864b-5d1ae06f976b";
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+
+            byte[] byteData = jpg.ToArray();
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                return new LiveCameraResult { Tags = response.Content };
+                //Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+
+            // Submit image to API. 
+          //  var analysis = await _visionClient.GetTagsAsync(jpg);
+           // // Count the API call. 
+           // Properties.Settings.Default.VisionAPICallCount++;
+            // Output. 
+          //  return new LiveCameraResult { Tags = analysis.Tags };
+        }
+
+        /// <summary> Function which submits a frame to the Computer Vision API for celebrity
+        ///     detection. </summary>
+        /// <param name="frame"> The video frame to submit. </param>
+        /// <returns> A <see cref="Task{LiveCameraResult}"/> representing the asynchronous API call,
+        ///     and containing the celebrities returned by the API. </returns>
+       
+
+        private BitmapSource VisualizeResult(VideoFrame frame)
+        {
+            // Draw any results on top of the image. 
+            BitmapSource visImage = frame.Image.ToBitmapSource();
+
+            var result = _latestResultsToDisplay;
+
+            if (result != null)
+            {
+                // See if we have local face detections for this image.
+               visImage = Visualization.DrawTags(visImage, result.Tags);
+            }
+
+            return visImage;
+        }
+
+        /// <summary> Populate CameraList in the UI, once it is loaded. </summary>
+        /// <param name="sender"> Source of the event. </param>
+        /// <param name="e">      Routed event information. </param>
+        private void CameraList_Loaded(object sender, RoutedEventArgs e)
+        {
+            int numCameras = _grabber.GetNumCameras();
+
+            if (numCameras == 0)
+            {
+                MessageArea.Text = "No cameras found!";
+            }
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = Enumerable.Range(0, numCameras).Select(i => string.Format("Camera {0}", i + 1));
+            comboBox.SelectedIndex = 0;
+        }
+
+        /// <summary> Populate ModeList in the UI, once it is loaded. </summary>
+        /// <param name="sender"> Source of the event. </param>
+        /// <param name="e">      Routed event information. </param>
+        private void ModeList_Loaded(object sender, RoutedEventArgs e)
+        {
+            var modes = (AppMode[])Enum.GetValues(typeof(AppMode));
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = modes.Select(m => m.ToString());
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void ModeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Disable "most-recent" results display. 
+            _fuseClientRemoteResults = false;
+
+            var comboBox = sender as ComboBox;
+            var modes = (AppMode[])Enum.GetValues(typeof(AppMode));
+            _mode = modes[comboBox.SelectedIndex];
+            switch (_mode)
+            {
+ 
+                case AppMode.Tags:
+                    _grabber.AnalysisFunction = TaggingAnalysisFunction;
+                    break;
+
+                default:
+                    _grabber.AnalysisFunction = null;
+                    break;
+            }
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CameraList.HasItems)
+            {
+                MessageArea.Text = "No cameras found; cannot start processing";
+                return;
+            }
+
+
+            // How often to analyze. 
+            _grabber.TriggerAnalysisOnInterval(Properties.Settings.Default.AnalysisInterval);
+
+            // Reset message. 
+            MessageArea.Text = "";
+
+            // Record start time, for auto-stop
+            _startTime = DateTime.Now;
+
+            await _grabber.StartProcessingCameraAsync(CameraList.SelectedIndex);
+        }
+
+        private async void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _grabber.StopProcessingAsync();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Visibility = 1 - SettingsPanel.Visibility;
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsPanel.Visibility = Visibility.Hidden;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        
+    }
+}
